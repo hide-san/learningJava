b6d8ed33699d44282417be4ac9ec59ef476f0133
hide-san <hideo@sato.name>
2014-10-17 08:08:50 +0900

    Array tests

--- test/learning/CharArrayTest.java
+++ test/learning/CharArrayTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CharArrayTest {
     @Test
@@ -13,5 +14,13 @@ public class CharArrayTest {
         Arrays.sort(chars);
 
         assertArrayEquals(new char[]{'1', '9', 'A', 'Z', 'a', 'z'}, chars);
+    }
+
+    @Test
+    public void testCharArrayToString() throws Exception {
+        char[] chars = {'H', 'i', 'd', 'e', 'o'};
+        String aString = new String(chars);
+
+        assertEquals("Hideo", aString);
     }
 }
